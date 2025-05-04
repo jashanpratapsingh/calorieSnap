f1b0d39c6dc1250e554979c2b29821f756a7c725
Jashan Pratap Singh <jashanpratap123@gmail.com>
2025-05-04 12:32:29 +0000

    Initial working module is ready

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${GeistSans.variable} ${GeistMono.variable} font-sans antialiased`}>
+      <body suppressHydrationWarning className={`${GeistSans.variable} ${GeistMono.variable} font-sans antialiased`}>
        <TanstackProvider>
           <SidebarProvider>
             <Header />
